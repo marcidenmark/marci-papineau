27cc1b296bc42dc932e1be10c31d9240750b34ef
marci papineau <marcidenmark@me.com>
2018-12-13 13:27:06 +0100

    fixed html { overflow-x: hidden;

--- scroll.js
+++ scroll.js
@@ -1,14 +1,4 @@
-// from le W.
 $(document).ready(function() {
-
-  // console.log("hello")
-    // the DOM is now ready, you can traverse it,
-    // and register events with on().
-    // the DOM is now ready, you can traverse it,
-    // and register events with on().
-
-// * is all ellements
-
     $(function() {
         var $animation_elements = $('.skillBarContainer > *');
 
@@ -28,20 +18,17 @@ $(document).ready(function() {
 
                 if ((element_bottom_position >= window_top_position) && (element_top_position <= window_bottom_position)) {
 
-                        $(this).addClass('skillBarValue');
-                    };
+                    $(this).addClass('skillBarValue');
+                };
             });
-            // console.log("--")
 
         });
-
     });
-
 });
 
 //console.log("second hello")
-                        //console.log("window_top_position: " + window_top_position)
-                        //console.log("element_bottom_position: " + element_bottom_position)
-                        // console.log("first condition: " + (element_bottom_position >= window_top_position))
-                        // console.log("element_top_position: " + element_top_position)
-                        // console.log("window_bottom_position: " + window_bottom_position)
+//console.log("window_top_position: " + window_top_position)
+//console.log("element_bottom_position: " + element_bottom_position)
+// console.log("first condition: " + (element_bottom_position >= window_top_position))
+// console.log("element_top_position: " + element_top_position)
+// console.log("window_bottom_position: " + window_bottom_position)
